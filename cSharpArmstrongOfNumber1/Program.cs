4602cd8b70951f1360b410303ecfc0bd3ff9cfcf
nimna nilwala <nimna.nilwala555@gmail.com>
2023-11-22 23:36:36 +0530

    Add project files.

---
+++ cSharpArmstrongOfNumber1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace cSharpArmstrongOfNumber1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, number2, rem=0, ams=0;
+            Console.Write("Enter the number...: ");
+            number = int.Parse(Console.ReadLine());
+            number2 = number;
+
+            while (number > 0)
+            {
+                rem = number % 10;
+                ams = ams + (rem * rem * rem);
+                number = number / 10;
+            }
+
+            if (number2 == ams)
+            {
+                Console.WriteLine(number2 + " is an Armstrong number.");
+            }
+            else
+            {
+                Console.WriteLine(number2 + " is not an Armstrong number.");
+            }
+        }
+    }
+}
